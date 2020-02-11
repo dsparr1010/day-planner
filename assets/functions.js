0c3d385ffe46e5888821b60147a04f3d07b4d49b
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-02-11 17:26:30 -0600

    bootstrap added to table

--- assets/functions.js
+++ assets/functions.js
@@ -2,35 +2,64 @@ $(document).ready(function () {
 
 $("#currentDate").text(moment().format('dddd,  MMMM Do YYYY'));    
 $('#currentTime').text(moment().format('h:mm a'));
-
-
-
 //9 am - 5 pm
-let hoursArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+let hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-hoursArray.forEach(function(hr) {
-    const time = $("<th id = 'time'>")
-    const content = $("<th id = 'content'>");
-    const saveBtn = $("<th id = 'saveBtn' class = 'is-right'><button>Save Button</button></th>");
+function renderTable() {
 
-   // $('#time').append(time);
-   // $('#content').append(content);
-   // $('#saveBtn').append(saveBtn);
-   $('#calendarIterate').append(time, content, saveBtn)
-});
+    hoursArray.forEach((hr) => {
+        const row = $(`<tr id = 'row${hr}'><th scope = 'row'>${hr}</th>`);
+        const time = $(`<td id = 'time${hr}' class = 'cal-time'>${hr} O'Clock</td>`);
+        const content = $(`<td id = 'content${hr}' 'class = 'cal-content' colspan = '4'><input id = 'input${hr} type = 'text' >What do you have planned?</input></td`);
+        const saveBtn = $(`<td id = 'saveBtn${hr}' class = 'cal-saveBtn is-right'><button for = 'input${hr}'>Save Button</button></td></tr>`);
+        const br = $('<br>');
 
-$.each({hoursArray}, function (key, value) {
-    console.log(this);
-    console.log(value);
-})
+        var inputID = $(`#input${hr}`).val();
+        var input = $(`#input${hr}`);
+        var btnInput = $(':button');
+
+    row.append(time, content, saveBtn, br)
+    $('#calendarIterate').append(row);
 
 
+    $(`#saveBtn${hr}`).on('click', () => {
+       // data.push(localStore);
+       // localStorage.setItem('data', JSON.stringify(data));   
+        console.log(`this btn: saveBtn${hr}`);
+    });
+
+
+
+    setStyle(hr);
+
+    });
+    
+}
+
+function setStyle(hr) {
+    const currentHour = moment().format('HH');
+
+    if (hr < currentHour) {
+        $(`#row${hr}`).addClass('table-danger');
+
+    } else if (hr > currentHour) {
+        $(`#row${hr}`).addClass('table-success');
+
+    } else {
+        $(`#row${hr}`).addClass('table-light');
+    }
+
+};
+
+var data = JSON.parse(localStorage.getItem('data')) || [];
+
+renderTable();
 
 }); //end doc ready func
 
 
 $(window).on('load', function() {
-const currentTime = moment().format('h:mm a');
+const currentTime = moment().format('HH:mm');
 const currentDay = moment().format('dddd,  MMMM Do YYYY');
 
 console.log(currentTime);  
